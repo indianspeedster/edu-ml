87e7c644669dea31a4a0883b2a19f4e68ff518df
Chameleon Cloud User <cc@sor-eduml.novalocal>
2023-08-13 21:58:34 +0000

    results added

---
+++ results/path_3/path_3.py
@@ -0,0 +1,20 @@
+from graphviz import Digraph
+
+
+G = Digraph(format='png')
+
+
+nodes = ['kfold_data', 'lower_bound', f'optimizer \n adamW', 'special_tokens', 'hyperparameter search \n scenario 2', "results"]
+for node in nodes:
+    G.node(node)
+
+
+edges = [('kfold_data', 'lower_bound'), ('lower_bound', f'optimizer \n adamW'), (f'optimizer \n adamW', 'special_tokens'), ('special_tokens', 'hyperparameter search \n scenario 2'), ('hyperparameter search \n scenario 2', 'results')]
+for edge in edges:
+    G.edge(*edge)
+
+
+
+
+
+G.render('path_3', cleanup=True, view=False)
