26160f2b442a76ed9e8323d7ae2045a1b1113206
Chameleon Cloud User <cc@sor-eduml.novalocal>
2023-08-15 11:43:32 +0000

    scenario changed

--- results/path_5/path_5.py
+++ results/path_5/path_5.py
@@ -4,12 +4,12 @@ from graphviz import Digraph
 G = Digraph(format='png')
 
 
-nodes = ['k_fold_data', 'upper_bound', f'optimizer \n adamW', 'hyperparameter search \n scenario 2', "results"]
+nodes = ['k_fold_data', 'upper_bound', f'optimizer \n adamW', 'hyperparameter search \n scenario 1', "results"]
 for node in nodes:
     G.node(node)
 
 
-edges = [('k_fold_data', 'upper_bound'), ('upper_bound', f'optimizer \n adamW'), (f'optimizer \n adamW',  'hyperparameter search \n scenario 2'), ('hyperparameter search \n scenario 2', 'results')]
+edges = [('k_fold_data', 'upper_bound'), ('upper_bound', f'optimizer \n adamW'), (f'optimizer \n adamW',  'hyperparameter search \n scenario 1'), ('hyperparameter search \n scenario 1', 'results')]
 for edge in edges:
     G.edge(*edge)
 
